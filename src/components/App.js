79b57e826d049ab7577d82f176a2043094ef16df
UsikovaVA <9004847432m@gmail.com>
2024-11-13 16:36:29 +0300

    оптимизация роутера

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Loader from './Loader/Loader';
 import CheckoutForm from './CheckoutForm/CheckoutForm'; 
 import PaymentPage from './PaymentPage/PaymentPage'; 
-import Certificates from './certificates/Сertificates';
 import '../components/App.css';
+import Certificates from './certificates/Сertificates';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Certificates />} />
-      <Route path="/checkout/:certificateId" element={<CheckoutForm />} />
-      <Route path="/payment" element={<PaymentPage />} />
+      <Route
+        path="/checkout/:certificateId"
+        element={
+          <Suspense fallback={<Loader />}>
+            <CheckoutForm />
+          </Suspense>
+        }
+      />
+      <Route
+        path="/payment"
+        element={
+          <Suspense fallback={<Loader />}>
+            <PaymentPage />
+          </Suspense>
+        }
+      />
     </Routes>
   );
 };
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,11 @@
+import './Loader.css';
+
+const Loader = () => {
+    return (
+        <div className="loader">
+            <h2 className="loader_title">Загрузка...</h2>
+        </div>
+    );
+};
+
+export default Loader;
